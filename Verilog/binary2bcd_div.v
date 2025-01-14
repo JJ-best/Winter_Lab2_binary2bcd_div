aad80e17711e091efbcf994439a3f86be304aaea
JJ-best <jesse.mojo@gmail.com>
2025-01-14 21:09:50 +0800

    v2

--- Verilog/binary2bcd_div.v
+++ Verilog/binary2bcd_div.v
@@ -28,13 +28,14 @@ module binary2bcd_div (
     output wire [15:0]packed_bcd //16-bit BCD(0~9999)
 );
 integer i;
-reg [13:0]a;
+reg [13:0]a[0:4];
 reg [3:0] digit[0:3];
+
 always @(*) begin
-    a = in_binary;
+    a[0][13:0] = in_binary;
     for (i = 0; i < 4; i = i + 1) begin
-        digit[i][3:0] = a % 10; //get LSB
-        a = a / 10;
+        digit[i][3:0] = a[i][13:0] % 10; //get LSB
+        a[i+1][13:0] = a[i][13:0] / 10;
     end
 end
 assign packed_bcd = {digit[3], digit[2], digit[1], digit[0]};
